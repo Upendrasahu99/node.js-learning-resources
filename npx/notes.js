a64a76f23b8b9ac04e50a839ed40832fb4f06b36
Upendra Sahu <upendrasahu1199@gmail.com>
2024-10-16 00:23:57 +0530

    npx notes

---
+++ npx/notes.js
@@ -0,0 +1,15 @@
+/** NPX
+ * Node package executer
+ * It come automatically with or above node version 5.2.0 
+ */
+
+/** NPM vs NPX
+ * NPM we can install, delete and update the packages.
+ * In npm we need to install package locally or globally than we can use but using npx we can direacly execute the package.
+ * NPM ex.
+ * npm install -g create-react-app
+ * create-react-app app-name
+ * 
+ * NPX ex.
+ * npx create-react-app app-name
+ */
